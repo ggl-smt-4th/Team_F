cbd97c96a0bd5cde6ad8860b7563fe34b7d7963c
Wei Sun <sunw@fxiaoke.com>
2018-06-19 21:37:15 +0800

    added in-class version

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,33 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract SinglePayroll {
+   uint salary = 10 wei;
+   address frank = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
+   uint payDuration = 5 seconds;
+   uint lastPayday;
+
+   function SinglePayroll() {
+       lastPayday = now;
+   }
+
+   function addFund() payable returns (uint) {
+       return this.balance;
+   }
+
+   function getPaid() {
+       uint nextPayday = lastPayday + payDuration;
+       if (nextPayday > now) {
+           revert();
+       }
+
+       if (this.balance/salary == 0) {
+           revert();
+       }
+
+       lastPayday = nextPayday;
+
+       frank.transfer(salary);
+   }
+
+
+}
