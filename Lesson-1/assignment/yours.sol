bd7b873f1389b1d11019897102e4be5608be1d5b
Wei Sun <sunw@fxiaoke.com>
2018-06-21 21:29:51 +0800

    added comment to answer to first assignment.

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,11 +1,16 @@
 pragma solidity ^0.4.14;
 
 contract SinglePayroll {
+	// the current employee's salary
    uint salary;
+   // the current employee
    address employee;
+   // the current employee's pay duration
    uint payDuration = 30 days;
+   // the current employee's last pay day
    uint lastPayday;
 
+   // constructor: initialize last pay day (actually this is not necessary)
    function SinglePayroll() {
        lastPayday = now;
    }
@@ -14,6 +19,9 @@ contract SinglePayroll {
        return this.balance;
    }
 
+
+   //update employee: 
+   //  do it only when the input address exists and differs from the existing one
    function updateEmployee(address e) {
 
        if (e == 0x0) {
@@ -29,6 +37,8 @@ contract SinglePayroll {
 
    }
 
+   //update salary
+   // do it when the input salary is different from the current value
    function updateSalary(uint s) {
         if (s == salary) {
             revert();
@@ -37,6 +47,9 @@ contract SinglePayroll {
         salary = s;
    }
 
+   // the employee get paid only when: 
+   //   1) the next pay day is not beyond now 
+   //   2) there is enough balance in the contract address
    function getPaid() {
 
        if (employee == 0x0) {
