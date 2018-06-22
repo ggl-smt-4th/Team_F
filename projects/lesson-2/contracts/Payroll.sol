5bb9034db88f942699caf57ecbf27fd4790ca644
Wei Sun <sunw@fxiaoke.com>
2018-06-22 20:57:56 +0800

    lesson-2: code and answer to home-work

--- projects/lesson-2/contracts/Payroll.sol
+++ projects/lesson-2/contracts/Payroll.sol
@@ -3,7 +3,9 @@ pragma solidity ^0.4.14;
 contract Payroll {
 
     struct Employee {
-        // TODO: your code here
+        address id;
+        uint salary;
+        uint lastPayDay;
     }
 
     uint constant payDuration = 30 days;
@@ -17,17 +19,55 @@ contract Payroll {
 
     function addEmployee(address employeeAddress, uint salary) public {
         require(msg.sender == owner);
-        // TODO: your code here
+        
+        employees.push(Employee(employeeAddress, salary * 1 ether, now));
+        
     }
 
     function removeEmployee(address employeeId) public {
         require(msg.sender == owner);
-        // TODO: your code here
+        
+        var (employee, index) = _findEmployeeWithAddress(employeeId);
+
+
+        assert (employee.id != 0x0);
+        
+        delete employees[index];
+        employees[index] = employees[employees.length - 1];
+        employees.length -= 1;
+
     }
+
+    function _findEmployeeWithAddress (address employeeAddress) private returns(Employee, uint)  {
+        for (uint i=0; i < employees.length; i++) {
+            if (employees[i].id == employeeAddress) {
+                return (employees[i], i);
+            }
+        }
+    }
+    
 
     function updateEmployee(address employeeAddress, uint salary) public {
         require(msg.sender == owner);
-        // TODO: your code here
+        
+        var (employee, index) = _findEmployeeWithAddress(employeeAddress);
+
+        assert (employee.id != 0x0);
+
+        uint salaryInETH = salary * 1 ether;
+        assert (employees[index].salary != salaryInETH);
+        
+        uint lastSalary = employees[index].salary;
+
+        employees[index].salary = salaryInETH;
+
+        uint remainingPayDay = (now - employees[index].lastPayDay);
+
+        employees[index].lastPayDay = now;
+
+        if (remainingPayDay > 0) {
+            employees[index].id.transfer(remainingPayDay / payDuration * lastSalary);
+        }
     }
 
     function addFund() payable public returns (uint) {
@@ -35,7 +75,15 @@ contract Payroll {
     }
 
     function calculateRunway() public view returns (uint) {
-        // TODO: your code here
+        uint totalSalary = 0;
+        for (uint i=0; i < employees.length; i++) {
+            totalSalary += employees[i].salary;
+        }
+
+        assert (totalSalary > 0);
+
+        return this.balance / totalSalary;
+        
     }
 
     function hasEnoughFund() public view returns (bool) {
@@ -43,7 +91,19 @@ contract Payroll {
     }
 
     function getPaid() public {
-        // TODO: your code here
+        var (employee, index) = _findEmployeeWithAddress(msg.sender);
+
+        assert (employee.id != 0x0);
+
+        uint nextPayDay = employee.lastPayDay + payDuration;
+        bool shouldPayNow = now >= nextPayDay;
+
+        assert (shouldPayNow);
+        
+        employees[index].lastPayDay = nextPayDay;
+
+        employee.id.transfer(employee.salary);
+
     }
 }
 
