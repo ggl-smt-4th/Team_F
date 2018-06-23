85bb77521c51248ecef9d3c6aff611e722f4fa8d
my-sunny <447564967@qq.com>
2018-06-23 23:20:49 +0800

    第二课作业

--- projects/lesson-2/contracts/Payroll.sol
+++ projects/lesson-2/contracts/Payroll.sol
@@ -2,91 +2,107 @@
 pragma solidity ^0.4.14;
 
 contract Payroll {
-    struct Employee{
+
+    struct Employee {
+        //your code here
         address id;
         uint salary;
         uint lastPayday;
     }
-    uint constant payDuration = 30 days;
+
+    uint constant payDuration = 30days;
 
     address owner;
     Employee[] employees;
-    uint totalSalary = 0;
     
-    function Payroll()payable public{
-        owner = msg.sender;
+    /*构造函数 */
+    function Payroll() payable public {
+        owner = msg.sender; 
     }
-    function _partialPaid(Employee employee)private{
-        uint payment = employee.salary * (now - employee.lastPayday) / payDuration;
-	    employee.id.transfer(payment);
+
+    /*支付工资 */
+    function _particlePaid(Employee employee) private{
+        uint payment = employee.salary * (now -employee.lastPayday) / payDuration;
+        employee.id.transfer(payment);
     }
     
-    function _findEmployee(address employeeId)private returns(Employee,uint){
-        for(uint i=0;i<employees.length;i++){
-            if(employees[i].id==employeeId){
-                return (employees[i],i);
-            } 
-            
+    //查找员工及所在位置
+    function _findEmployee(address newAddr) private returns (Employee, uint) {
+        for (uint i=0; i<employees.length; i++){
+            if (employees[i].id == newAddr){
+                return (employees[i], i);
+            }
         }
     }
     
-    function addEmployee(address employeeId, uint salary)public{
+    /*添加员工*/
+    function addEmployee(address employeeAddress, uint salary) public {
         require(msg.sender == owner);
-        var (employee,index) = _findEmployee(employeeId);
+        // TODO: your code here
+        var (employee,index) = _findEmployee(employeeAddress);
         assert(employee.id == 0x0);
-        totalSalary = totalSalary+salary * 1 ether;
-        employees.push(Employee(employeeId,salary * 1 ether,now));
+        
+        employees.push(Employee(employeeAddress,salary * 1 ether,now));
     }
-    function removeEmployee(address employeeId)public{
+
+     /*删除员工*/
+    function removeEmployee(address employeeId) public {
         require(msg.sender == owner);
-         var (employee,index) = _findEmployee(employeeId);
-         assert(employee.id != 0x0);
-         _partialPaid(employee);
-         //删除员工时减去他的薪水
-          totalSalary -=employees[index].salary;
-         delete employees[index]; 
-         employees[index] = employees[employees.length-1];
-         employees.length-1;
+        // TODO: your code here
+        var (employee,index) = _findEmployee(employeeId);
+        assert(employee.id != 0x0);
         
+        delete employees[index];
+        employees[index] = employees[employees.length - 1];
+        employees.length -= 1;
     }
 
-    
-
-	function updateEmployee(address employeeId, uint salary) public{
-	    require(msg.sender == owner);
-	     var (employee,index) = _findEmployee(employeeId);
-	      assert(employee.id != 0x0);
-	      _partialPaid(employee);
-	      //先减去变更的薪水
-	      totalSalary -=employees[index].salary;
-          employees[index].salary = salary * 1 ether;
-          //加上变更后的薪水
-           totalSalary +=employees[index].salary;
-          employees[index].lastPayday = now;
-	    
-     
+    /*更新员工信息*/
+    function updateEmployee(address employeeAddress, uint salary) public {
+        require(msg.sender == owner);
+        // TODO: your code here
+        var (employee,index) = _findEmployee(employeeAddress);
+        assert(employee.id != 0x0);
+        
+        _particlePaid(employee);
+        employee.id = employeeAddress;
+        employee.salary = salary * 1 ether;
+        employee.lastPayday = now;
     }
-    
+
     function addFund() payable public returns (uint) {
-        return this.balance;
+        return address(this).balance;
     }
-    
-    function calculateRunway()public view returns (uint) {
+
+    function calculateRunway() public view returns (uint) {
+        // TODO: your code here
+        uint totalSalary = 0;
+        for (uint i=0; i < employees.length; i++) {
+            totalSalary += employees[i].salary;
+        }
+
+        assert (totalSalary > 0);
+
+        return this.balance / totalSalary;
         
-        return this.balance / totalSalary ;
     }
-    
-    function hasEnoughFund()public view returns (bool) {
+
+    function hasEnoughFund() public view returns (bool) {
         return calculateRunway() > 0;
     }
-    
-    function getPaid() public{
-      var (employee,index) = _findEmployee(msg.sender);
-	  assert(employee.id != 0x0);
-     
-      uint nextPayday = employee.lastPayday+payDuration;
-      assert(nextPayday < now); 
-      employees[index].lastPayday = nextPayday;
-      employee.id.transfer(employee.salary);
+
+    function getPaid() public {
+        // TODO: your code here
+        var (employee, index) = _findEmployee(msg.sender);
+
+        assert (employee.id != 0x0);
+
+        uint nextPayDay = employee.lastPayday + payDuration;
+
+        assert (nextPayDay <= now);
+        
+        employees[index].lastPayday = nextPayDay;
+
+        employee.id.transfer(employee.salary);
     }
 }
