8e818a9edf263dfd6210ca633526ff2dcb518fe6
Wei Sun <sunwei415@126.com>
2018-06-30 13:05:49 +0800

    first runnable unit test

--- projects/lesson-4/test/add_employee.spec.js
+++ projects/lesson-4/test/add_employee.spec.js
@@ -6,18 +6,17 @@ contract('Payroll', function (accounts) {
 
     var employee = accounts[1];
 
-    xit('should run', function () {
+    it('should run', function () {
         var payroll;
         Payroll.deployed().then(function (instance) {
             payroll = instance;
             payroll.addEmployee(employee, 1);
         }).then(function () {
-            payroll.addFund.call(owner, {from: owner, value: web3.toWei(30, 'ether')})
-            //     .then(() => {
-            //     return payroll.calculateRunway();
-            // }).then(runway => {
-            //     assert.equal(runway, 30, "runway should beo 30 / 1");
-            // });
-        })
+            payroll.addFund({from: owner, value: web3.toWei(30, 'ether')});
+        }).then(() => {
+            return payroll.calculateRunway();
+        }).then(runway => {
+            assert.equal(runway, 30, "runway should beo 30 / 1");
+        });
     });
 });
