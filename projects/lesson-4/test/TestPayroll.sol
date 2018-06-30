719803b2becedf318eb7a079d56dd60703026424
Wei Sun <sunwei415@126.com>
2018-06-30 09:04:53 +0800

    contract test written in solidity is not good for integration test

--- projects/lesson-4/test/TestPayroll.sol
+++ projects/lesson-4/test/TestPayroll.sol
@@ -4,8 +4,12 @@ import "truffle/Assert.sol";
 import "truffle/DeployedAddresses.sol";
 import "../contracts/Payroll.sol";
 
-contract TestPayroll {
+contract TestPayroll is Payroll {
     function TestPayroll(){
 
+    }
+
+    function testAddEmployee() {
+  
     }
 }
