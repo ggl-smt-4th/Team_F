384c5c3a9cbb1bde7e74053529b4f2d91dcae9ed
Wei Sun <sunwei415@126.com>
2018-06-30 08:57:42 +0800

    wip setting unit test with solidity.

---
+++ projects/lesson-4/test/TestPayroll.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.0;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/Payroll.sol";
+
+contract TestPayroll {
+    function TestPayroll(){
+
+    }
+}
