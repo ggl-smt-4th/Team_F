b8d2c937592b56b29970c39f731b43ca5cc64af2
yiyanwannian <774392980@qq.com>
2018-07-01 22:51:40 +0800

    update homework 4

--- projects/lesson-4/migrations/2_deploy_contracts.js
+++ projects/lesson-4/migrations/2_deploy_contracts.js
@@ -1,2 +1,9 @@
-module.exports = function (deployer) {
+var SafeMathLib = artifacts.require("../contracts/SafeMath.sol");
+var Payroll = artifacts.require("../contracts/Payroll.sol");
+
+module.exports = function(deployer) {
+  deployer.deploy(SafeMathLib);
+  deployer.link(SafeMathLib, Payroll);
+  deployer.deploy(Payroll);
 };
+
