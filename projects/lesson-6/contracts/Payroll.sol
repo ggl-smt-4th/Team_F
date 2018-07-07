ba1852cf9d8b3c13d1a226399a139d4d252638ff
whynotyes <whynotyes@whynotyess-MacBook-Pro.local>
2018-07-07 22:34:36 +0800

    Feat: 第六课作业

--- projects/lesson-6/contracts/Payroll.sol
+++ projects/lesson-6/contracts/Payroll.sol
@@ -35,6 +35,22 @@ contract Payroll is Ownable {
     uint public totalSalary = 0;
     address[] employeeAddressList;
 
+    event NewEmployee (
+        address employee
+    );
+    event UpdateEmployee (
+        address employee
+    );
+    event RemoveEmployee (
+        address employee
+    );
+    event NewFund (
+        uint balance
+    );
+    event GetPaid (
+        address employee
+    );
+
     /**
      * This contract is simple, We update employees by the key directly
      * instead of updating a copy so that we could save some gas.
@@ -60,6 +76,7 @@ contract Payroll is Ownable {
         employees[employeeId] = Employee(index, salary, now);
 
         totalSalary = totalSalary.add(salary);
+        NewEmployee(employeeId);
     }
 
     function removeEmployee(address employeeId) public onlyOwner shouldExist(employeeId) {
@@ -80,6 +97,7 @@ contract Payroll is Ownable {
 
         // adjust length
         employeeAddressList.length -= 1;
+        RemoveEmployee(employeeId);
     }
 
     function changePaymentAddress(address oldAddress, address newAddress) public onlyOwner shouldExist(oldAddress) shouldNotExist(newAddress) {
@@ -98,9 +116,11 @@ contract Payroll is Ownable {
         employees[employeeId].salary = salary;
         employees[employeeId].lastPayday = now;
         totalSalary = totalSalary.add(salary).sub(oldSalary);
+        UpdateEmployee(employeeId);
     }
 
     function addFund() payable public returns (uint) {
+        NewFund(address(this).balance);
         return address(this).balance;
     }
 
@@ -123,6 +143,7 @@ contract Payroll is Ownable {
 
         employees[employeeId].lastPayday = nextPayday;
         employeeId.transfer(employees[employeeId].salary);
+        GetPaid(employeeId);
     }
 
     function getEmployerInfo() view public returns (uint balance, uint runway, uint employeeCount) {
--- projects/lesson-6/src/components/Common.js
+++ projects/lesson-6/src/components/Common.js
@@ -9,17 +9,28 @@ class Common extends Component {
   }
 
   componentDidMount() {
-    const { payroll } = this.props;
+    const { payroll, web3 } = this.props;
     const updateInfo = (error, result) => {
       if (!error) {
         this.getEmployerInfo();
       }
     }
 
+    this.newFund = payroll.NewFund(updateInfo);
+    this.getPaid = payroll.GetPaid(updateInfo);
+    this.newEmployee = payroll.NewEmployee(updateInfo);
+    this.updateEmployee = payroll.UpdateEmployee(updateInfo);
+    this.removeEmployee = payroll.RemoveEmployee(updateInfo);
+
     this.getEmployerInfo();
   }
 
   componentWillUnmount() {
+    this.newFund.stopWatching();
+    this.getPaid.stopWatching();
+    this.newEmployee.stopWatching();
+    this.updateEmployee.stopWatching();
+    this.removeEmployee.stopWatching();
   }
 
   getEmployerInfo = () => {
